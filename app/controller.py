3d5d735050e61497cf70058d859b4ce0ee9c78d1
Alan <alanoswalt01@gmail.com>
2026-07-26 18:32:57 -0600

    calling main page two times

--- app/controller.py
+++ app/controller.py
@@ -6,10 +6,9 @@ from ui.main_menu import main_window
 class Save_Password:
     def __init__(self) -> None:
         self.login_window_object = login_window()
-        self.name_of_user = self.login_window_object.user_name_app
+        self.name_of_user = self.login_window_object.gui()
         self.main_window_object = main_window(self.name_of_user)
-        self.run_main_window()
-    
+
     def run_main_window(self):
         while True:
             self.main_window_object.gui()
--- ui/login.py
+++ ui/login.py
@@ -6,71 +6,65 @@ from database.all_users_db import all_users_database
 
 class login_window:
     def __init__(self) -> None:
-        self.gui_login_page ='''
-                
+        self.gui_login_page = '''
                 Enter a number
                 1. Log in
-                2. Sing Up
+                2. Sign Up
                 3. Exit
             '''        
         self.user_name_app = ""
-        self.valid_user_in_database = False
         self.user_db = all_users_database("all_user")
-        self.gui()
 
     def gui(self):
-        '''
-        user_input: Number to chose what to do
-        user_email: user name
-        password: password of account
-        user_exists: True if account is in data base
-        question_create_one: if account doesn't exists ask if you want to create one
-        
-        '''
-        print(self.gui_login_page)
-        user_input = input("Please enter your answer: ")
-        user_email = input("Please enter your user: ")
-        password = input("Please enter your password: ")
-        
-        if user_input == '1':
-            log.info("Looking for user")
-            for i in range(2):
-                user_exists = self.user_db.look_for_user(user_email)
-                if user_exists:
-                    log.info("User exist, comparing password")
-                    correct_password = self.user_db.compare_password(user_email, password)
-                    
-                    if not correct_password:
-                        print("Provide correct credentials")
-                        user_email = input("Please enter your user: ")
-                        password = input("Please enter your password: ")
-                    else:
-                        break
-                        
-                else:
-                    print("User doesn't exists, create one?")
-                    question_create_one = input("Please enter your answer: yes/no")
-                    if question_create_one.lower().strip() == "yes":
-                        log.info("Adding new user")
-                        self.user_db.add_new_user(user_email, password)
-                        break
-                    else:
-                        print("Login with correct user")
-                        sys.exit()
+        """Run the login menu until authentication succeeds or the user exits."""
+        while True:
+            print(self.gui_login_page)
+            user_input = input("Please enter your answer: ").strip()
 
-            else:
-                print('Closing app please provide correct input')
+            # Exit before asking for credentials.
+            if user_input == "3":
+                print("Goodbye")
                 sys.exit()
-                
 
-        elif user_input == '2':
-            user_exists = self.user_db.look_for_user(user_email)
-            if user_exists:
-                print("User already exists, please login")
-            else:
-                print("User doesn't exists, create one")
-                self.user_db.add_new_user(user_email, password)
-        elif user_input == '3':
-            sys.exit()
+            if user_input not in {"1", "2"}:
+                print("Invalid option. Please choose 1, 2, or 3.")
+                continue
+
+            user_email = input("Please enter your user: ").strip()
+            password = input("Please enter your password: ")
+
+            if not user_email or not password:
+                print("User and password are required.")
+                continue
+
+            if user_input == "2":
+                new_user = self._sign_up(user_email, password)
+                if new_user:
+                    return new_user
+                continue
+
+            if self._log_in(user_email, password):
+                return self.user_name_app
+
+    def _sign_up(self, user_email, password):
+        if self.user_db.look_for_user(user_email):
+            print("User already exists. Please log in.")
+            return None
+
+        log.info("Adding new user")
+        self.user_db.add_new_user(user_email, password)
+        print("Account created successfully.")
         self.user_name_app = user_email
-        print(self.user_name_app)
+        return self.user_name_app
+
+    def _log_in(self, user_email, password):
+        if not self.user_db.look_for_user(user_email):
+            print("User does not exist. Please sign up first.")
+            return False
+
+        if self.user_db.compare_password(user_email, password):
+            self.user_name_app = user_email
+            return True
+
+        print("Incorrect credentials.")
+        return False
